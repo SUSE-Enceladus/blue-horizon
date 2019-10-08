292a9d34229aa2b0b947e959d81f669021444d65
James Mason <jmason@suse.com>
2019-10-08 08:24:23 -0700

    Update sidebar with active (mock) routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,29 @@
 module ApplicationHelper
   def sidebar_icons
     {
-      'Welcome'         => 'announcement',
-      'Cluster size'    => 'photo_size_select_small',
-      'Additional data' => 'playlist_add',
-      'Custom/Advanced' => 'details',
-      'Plan'            => 'organization',
-      'Deploy'          => 'play_arrow',
-      'Download'        => 'file_download'
+      welcome: 'announcement',
+      cluster: 'photo_size_select_small',
+      framework: 'playlist_add',
+      advanced: 'configuration_file',
+      plan: 'organization',
+      deploy: 'play_arrow',
+      download: 'file_download'
     }
   end
 
-  def sidebar_menu_item(text, icon, active=false)
-    content_tag(:a,
-      class: ['list-group-item', (active ? 'active' : '')],
-      data:  { toggle: 'tooltip', placement: 'right', original_title: text }
-    ) do
-      content_tag(:i, icon, class: ['eos-icons', 'md-18']) +
-      ' ' +
+  def sidebar_menu_item(path_key, active=false)
+    text = t("sidebar.#{path_key}")
+    icon = sidebar_icons[path_key]
+    url = "/#{path_key}"
+
+    content = [
+      content_tag(:i, icon, class: ['eos-icons', 'md-18']),
       content_tag(:span, text, class: 'collapse')
-    end
+    ].join(' ').html_safe
+
+    active_link_to(content, url,
+      class: 'list-group-item',
+      data:  { toggle: 'tooltip', placement: 'right', original_title: text }
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # mock routes
+  get '/welcome', to: 'welcome#index'
+  get '/cluster', to: 'welcome#index'
+  get '/framework', to: 'welcome#index'
+  get '/advanced', to: 'welcome#index'
+  get '/plan', to: 'welcome#index'
+  get '/deploy', to: 'welcome#index'
+  get '/download', to: 'welcome#index'
 end
