acff6a290d381be1eaafb3668e89fb4095514f26
James Mason <jmason@suse.com>
2019-10-22 14:25:30 -0700

    Implement cluster size UI

---
+++ app/controllers/clusters_controller.rb
@@ -0,0 +1,25 @@
+class ClustersController < ApplicationController
+  def show
+    @cluster = ClusterSizeSliderDecorator.new(Cluster.load)
+    @instance_types = Cloud::InstanceType.for(@cluster.cloud_framework)
+  end
+
+  def update
+    @cluster = Cluster.new(cluster_params)
+    if @cluster.save
+      redirect_to framework_path
+    else
+      render :show, flash: {
+        error: @cluster.errors.full_messages.to_sentence
+      }
+    end
+  end
+
+  def cluster_params
+    safe_params = params.require(:cluster).permit(
+      :cloud_framework, :instance_type, :instance_type_custom, :instance_count,
+    )
+    safe_params['cloud_framework'] = KeyValue.get(:cloud_framework)
+    return safe_params
+  end
+end
