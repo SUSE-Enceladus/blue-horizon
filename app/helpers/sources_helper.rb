6622ff1809da28dd3f7243d283111fd9881854b8
James Mason <jmason@suse.com>
2019-12-18 08:31:02 -0800

    Move supported source file types into configuration.

--- app/helpers/sources_helper.rb
+++ app/helpers/sources_helper.rb
@@ -2,14 +2,13 @@
 
 # Helpers specific to editing sources
 module SourcesHelper
+  # Return an ace highlighter mode, based on filename.
+  # Default to "ace/mode/terraform".
   def ace_highlighter_for(filename)
-    mode =
-      case File.extname(filename)
-      when '.sh'
-        'sh'
-      else
-        'terraform'
-      end
+    mode = Rails.configuration.x.supported_source_extensions[
+      File.extname(filename)
+    ]
+    mode ||= 'terraform'
     return "ace/mode/#{mode}"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,30 @@
 # The data can then be loaded with the rails db:seed command
 # (or created alongside the database with db:setup).
 
+Rails.logger = Logger.new(STDOUT)
+Rails.logger.level = Logger::INFO
+
 # Populate KeyValues
 if ENV['CLOUD_FRAMEWORK'].present?
   KeyValue.set(:cloud_framework, ENV['CLOUD_FRAMEWORK'])
 end
 
 # Populate editable sources from the static documents
-Dir.glob(Rails.root.join('vendor', 'sources', '*')).each do |filepath|
-  filename = filepath.split('/').last
-  next if Source.find_by(filename: filename)
+def import_sources(glob)
+  glob.each do |filepath|
+    filename = filepath.split('/').last
+    next if Source.find_by(filename: filename)
 
-  Source.create(
-    filename: filename,
-    content:  File.read(filepath)
-  )
+    Rails.logger.info("New source file '#{filename}'")
+    Source.create(
+      filename: filename,
+      content:  File.read(filepath)
+    )
+  end
+end
+
+sources_path = ENV['TERRAFORM_SOURCES_PATH']
+sources_path ||= Rails.root.join('vendor', 'sources')
+Rails.configuration.x.supported_source_extensions.keys.each do |ext|
+  import_sources(Dir.glob(File.join(sources_path, "*#{ext}")))
 end
