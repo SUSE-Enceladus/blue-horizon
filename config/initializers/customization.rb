0f7f672895bdeed97e4aff63992d83de12861d04
James Mason <jmason@suse.com>
2019-12-18 09:52:10 -0800

    Add framework for customized configuration

--- config/initializers/customization.rb
+++ config/initializers/customization.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
+  custom_config_path = ENV['BLUE_HORIZON_CUSTOMIZER']
+  custom_config_path ||= Rails.root.join('vendor', 'customization.yml')
+
+  if File.exist? custom_config_path
+    customizer = YAML.load_file(custom_config_path)
+    config.x.merge(customizer)
+  end
+
+  # Customizations
+
   # Export path for modified source files (where terraform will run)
+  # In customization.yml:
+  # source_export_dir: /path/to/working/dir
   # defaults to tmp/terraform
-  # config.x.source_export_dir = Rails.root.join('tmp', 'terraform')
 end
 
 # The following performs required actions based on custom configuration above
