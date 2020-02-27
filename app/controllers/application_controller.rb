f4642e7dff59d527817332398e2fb3156f129c0f
James Mason <jmason@suse.com>
2020-02-26 22:47:04 -0800

    Use authorization to prevent skipping ahead

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@
 # Abstract class for centralizing ActionController customization
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authorize
+
+  def authorize
+    return if helpers.can(request.path)
+
+    flash[:error] = t(:unauthorized)
+    redirect_back(fallback_location: welcome_path)
+  end
 end
---
+++ app/helpers/authorization_helper.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# Authorize access to steps
+module AuthorizationHelper
+  def can(path)
+    result = case path
+    when plan_path
+      all_variables_are_set?
+    when deploy_path
+      plan_exists?
+    when download_path
+      plan_exists? && apply_log_exists?
+    else
+      true
+    end
+    logger.debug "AUTH: #{result ? 'can' : 'cannot'} access #{path}"
+    return result
+  end
+
+  private
+
+  def all_variables_are_set?
+    Variable.load.attributes.values.all?(&:present?)
+  end
+
+  def export_file_exists?(filename)
+    path = Rails.configuration.x.source_export_dir.join(filename)
+    File.exist?(path)
+  rescue StandardError
+    false
+  end
+
+  def plan_exists?
+    export_file_exists?('current_plan')
+  end
+
+  def apply_log_exists?
+    export_file_exists?('tf-apply.log')
+  end
+end
