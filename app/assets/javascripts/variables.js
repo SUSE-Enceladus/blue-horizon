435d39975b6e96704ff054c962bb6b1aacf531b0
James Mason <jmason@suse.com>
2020-02-27 12:29:25 -0800

    Fix up list & map variable entry
    * Prevent buttons from submitting form
    * Handle Enter key for continuous entry
    
    Resolves https://github.com/SUSE-Enceladus/blue-horizon/issues/52

--- app/assets/javascripts/variables.js
+++ app/assets/javascripts/variables.js
@@ -3,4 +3,10 @@ $(function(){
 	$('form#new_variable').on('click', ".remove", function(){
 		$(this).closest(".input-group").remove();
 	});
+	// Prevent submitting with Enter key
+	$(document).on("keydown", ":input:not(textarea)", function(event) {
+    if (event.key == "Enter") {
+        event.preventDefault();
+    }
+	});
 });
