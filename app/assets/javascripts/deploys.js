e6c321814c7860e7439e2f192ccc7b68100a1255
James Mason <jmason@suse.com>
2020-02-28 12:00:15 -0800

    Use  for JS linting

--- app/assets/javascripts/deploys.js
+++ app/assets/javascripts/deploys.js
@@ -1,70 +1,70 @@
 $(function() {
   var intervalId = undefined;
   var finished = false;
-  $('.eos-icon-loading').hide();
+  $(".eos-icon-loading").hide();
 
-  $('#submit-deploy')
-    .bind('ajax:beforeSend', function() {
-      $('#output').text('');
-      $(this).addClass('no-hover');
-      $('.btn-secondary').addClass('disabled');
-      $("a[href='/download']").addClass('disabled');
-      $('.eos-icon-loading').show();
+  $("#submit-deploy")
+    .bind("ajax:beforeSend", function() {
+      $("#output").text("");
+      $(this).addClass("no-hover");
+      $(".btn-secondary").addClass("disabled");
+      $("a[href='/download']").addClass("disabled");
+      $(".eos-icon-loading").show();
       intervalId = setTimeout(function() {
-	      fetch_output(finished, intervalId)
-      }, 5000)
+        fetch_output(finished, intervalId);
+      }, 5000);
     })
-    .bind('ajax:success', function() {
-      $('#notice').html("<%= flash[:error] %>");
-      if ($('#output').text().length > 0) {
-	      $('.eos-icon-loading').addClass('hide');
-	      clearTimeout(intervalId);
+    .bind("ajax:success", function() {
+      $("#notice").html("<%= flash[:error] %>");
+      if ($("#output").text().length > 0) {
+        $(".eos-icon-loading").addClass("hide");
+        clearTimeout(intervalId);
       }
       finished = true;
     })
-    .bind('ajax:complete', function() {
-      $('.btn-secondary').removeClass('disabled');
-      $("a[href='/download']").removeClass('disabled');
-      $(this).removeClass('no-hover');
-      if ($('#output').text().length > 0) {
-	      $('.eos-icon-loading').hide();
+    .bind("ajax:complete", function() {
+      $(".btn-secondary").removeClass("disabled");
+      $("a[href='/download']").removeClass("disabled");
+      $(this).removeClass("no-hover");
+      if ($("#output").text().length > 0) {
+        $(".eos-icon-loading").hide();
         clearTimeout(intervalId);
-	      finished = true;
+        finished = true;
       }
     })
-    .bind('ajax:error', function() {
-      $('.eos-icons-loading').hide();
+    .bind("ajax:error", function() {
+      $(".eos-icons-loading").hide();
       clearTimeout(intervalId);
     });
 });
 
 function fetch_output(finished, intervalId) {
   $.ajax({
-    type: 'GET',
-    url: 'deploy/send_current_status',
-    dataType: 'json',
+    type: "GET",
+    url: "deploy/send_current_status",
+    dataType: "json",
     success: function(data) {
-      if(data.success == false) {
-        $('.eos-icon-loading').hide();
+      if (data.success == false) {
+        $(".eos-icon-loading").hide();
         // show rails flash message
-        $('#error_message').text('Deploy operation has failed.');
-        $('#flash').show();
+        $("#error_message").text("Deploy operation has failed.");
+        $("#flash").show();
         // show terraform error message in output section
-        $('#output').text($('#output').text() + data.error);
+        $("#output").text($("#output").text() + data.error);
         clearTimeout(intervalId);
       } else {
-        $('.pre-scrollable').html(data.new_html);
+        $(".pre-scrollable").html(data.new_html);
         if (!finished) {
-      	  intervalId = setTimeout(function() {
-	          fetch_output();
-	        }, 5000);
-	      } else {
-	        $('.eos-icon-loading').addClass('hide');
+          intervalId = setTimeout(function() {
+            fetch_output();
+          }, 5000);
+        } else {
+          $(".eos-icon-loading").addClass("hide");
         }
       }
     },
     error: function() {
-      console.log('Error calling deploy/send_current_status');
+      console.log("Error calling deploy/send_current_status");
     }
   });
 }
--- app/assets/javascripts/plans.js
+++ app/assets/javascripts/plans.js
@@ -1,17 +1,17 @@
 $(function() {
-  $('#submit-plan')
-    .bind('ajax:beforeSend', function() {
-      $('code.output').text('');
-      $(this).addClass('no-hover');
-      $('.eos-icon-loading').show();
+  $("#submit-plan")
+    .bind("ajax:beforeSend", function() {
+      $("code.output").text("");
+      $(this).addClass("no-hover");
+      $(".eos-icon-loading").show();
     })
-    .bind('ajax:success', function(evt) {
-      $('code.output').text(evt.detail[0]);
+    .bind("ajax:success", function(evt) {
+      $("code.output").text(evt.detail[0]);
     })
-    .bind('ajax:complete', function() {
-      $(this).removeClass('no-hover');
-      $("a[href='/deploy']").removeClass('disabled');
-      $('.eos-icon-loading').addClass('hide');
-      $('.btn-info').removeClass('disabled');
+    .bind("ajax:complete", function() {
+      $(this).removeClass("no-hover");
+      $("a[href='/deploy']").removeClass("disabled");
+      $(".eos-icon-loading").addClass("hide");
+      $(".btn-info").removeClass("disabled");
     });
 });
