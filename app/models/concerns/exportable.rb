50416958a16be9701b615414a5d94148da585239
James Mason <jmason@suse.com>
2020-04-10 12:36:07 -0700

    Add export path function in Exportable

--- app/models/concerns/exportable.rb
+++ app/models/concerns/exportable.rb
@@ -4,11 +4,21 @@
 module Exportable
   EXECUTABLE = ['.sh'].freeze
 
-  def export_into(path, filename=self.filename)
-    File.write(File.join(path, filename), content)
+  def export_path(
+    path=Rails.configuration.x.source_export_dir,
+    filename=self.filename
+  )
+    File.join(path, filename)
+  end
+
+  def export_into(
+    path=Rails.configuration.x.source_export_dir,
+    filename=self.filename
+  )
+    File.write(export_path(path, filename), content)
     return unless EXECUTABLE.include?(File.extname(filename))
 
-    FileUtils.chmod('+x', File.join(path, filename))
+    FileUtils.chmod('+x', export_path(path, filename))
   end
 
   def export
