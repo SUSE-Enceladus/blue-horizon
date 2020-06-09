6e6587a976088a1bfb546497ed090fa95c35c22d
Jesus Bermudez Velazquez <jesus.bv@suse.com>
2020-06-09 19:07:33 +0100

    Fix script validation
    When making a typo in the scripts,
    the validation fails not allowing
    to correct it.
    
    This Fixes #111
    
    Handle terraform init error

--- app/services/terraform.rb
+++ app/services/terraform.rb
@@ -13,19 +13,21 @@ class Terraform
         backend:  false,
         no_color: true
       )
+    rescue RubyTerraform::Errors::ExecutionError
+      @logger.error('Error calling terraform init.')
     end
   end
 
   def config_terraform
-    logger = Logger.new(
+    @logger = Logger.new(
       RubyTerraform::MultiIO.new(STDOUT, log_file),
       level: :debug
     )
     RubyTerraform.configure do |config|
       config.binary = find_default_binary
-      config.logger = logger
-      config.stdout = logger
-      config.stderr = logger
+      config.logger = @logger
+      config.stdout = @logger
+      config.stderr = @logger
     end
   end
 
@@ -86,7 +88,7 @@ class Terraform
     start += 'Error: '.length
     limit = message[start, message.length].index("\n")
     parsed_message = message[start, limit]
-    line = message =~ /line [0-9]+,/
+    line = message =~ /line [0-9]+/
 
     parsed_message += add_filename(message) if file
     parsed_message += " in #{message[line, 6]}:"
@@ -96,14 +98,12 @@ class Terraform
   end
 
   def add_filename(error_message)
-    export_dir = Rails.configuration.x.source_export_dir.to_s
-    export_dir = export_dir[Rails.root.to_s.length + 1, export_dir.length]
-    local_dir = error_message.index(export_dir)
-    filename = error_message.slice(local_dir, error_message.length)
-    boundary = export_dir.length + 1
-    filename = filename.slice(boundary, filename.index(' ') - boundary)
-
-    return " on script '#{filename}'"
+    source_files = Dir[Rails.configuration.x.source_export_dir.to_s + '/*']
+    i = 0
+    i += 1 until error_message.index(File.basename(source_files[i])) ||
+                 i >= error_message.length
+    return " on script '#{File.basename(source_files[i])}'" if
+      i < error_message.length
   end
 
   def self.statefilename
